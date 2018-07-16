81b43fa097dbacc6fe1d173d21fe12dcfe61b6fa
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-07-16 12:56:10 -0300

    scanner de portas

--- ToolsMan.py
+++ ToolsMan.py
@@ -1,12 +1,39 @@
+from platform import system
+import socket
 import subprocess as sp
+print('Seu SO é {}'.format(system()))
 
+ip = input("Qual o ip do alvo: ")
 
-ip = input('Entre com o ip do alvo: ')
-status,result = sp.getstatusoutput("ping -c1 -w2 " + ip + ">> /dev/null")
+if system() == "Windows":
+    print("-" * 60)
+    print('Aguarde...')
+    status, result = sp.getstatusoutput("ping -c1 -w2 " + ip + " > NUL")
+
+else:
+    status, result = sp.getstatusoutput("ping -c1 -w2 " + ip + " >> /dev/null")
+    #status, result = sp.getstatusoutput("ping -c1 -w2 " + ip)
 
 if status == 0:
-    print("System " + ip + " is UP !")
+    print("-"*60)
+    print("Sistema " + ip + " is UP !")
+    
 else:
-    print("System " + ip + " is DOWN !")
+    print("-" * 60)
+    print("Sistema " + ip + " is DOWN !")
+    print(result)
 
-print(result)
+
+#TESTANDO CONEXAO POR PORTA DO ALVO
+port = int(input('Enter port: '))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((ip, port))
+    s.shutdown(2)
+    print("Success connecting to ")
+    print(ip," on port: ",str(port))
+except socket.error as e:
+    print("Cannot connect to ")
+    print(ip," on port: ",str(port))
+    print(e)
