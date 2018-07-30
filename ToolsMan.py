90d5bc8b8bbf289b00c4b4363bea2e8cfec54a2c
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-07-30 20:00:41 -0300

    alt

--- ToolsMan.py
+++ ToolsMan.py
@@ -1,8 +1,9 @@
 from platform import system
 import socket
 import subprocess as sp
+print("-" * 20)
 print('Seu SO é {}'.format(system()))
-
+print("-" * 20)
 ip = input("Qual o ip do alvo: ")
 
 if system() == "Windows":
@@ -16,23 +17,23 @@ else:
 if status == 0:
     print("-"*60)
     print("Sistema " + ip + " is UP !")
-    
+
+    #Test per port
+    port = int(input('Enter port: '))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((ip, port))
+        s.shutdown(2)
+        print("Success connecting to ")
+        print(ip," on port: ",str(port))
+    except socket.error as e:
+        print("Cannot connect to ")
+        print(ip," on port: ",str(port))
+        #print(e)
+
 else:
     print("-" * 60)
     print("Sistema " + ip + " is DOWN !")
     print(result)
 
-
-#TESTANDO CONEXAO POR PORTA DO ALVO
-port = int(input('Enter port: '))
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    s.connect((ip, port))
-    s.shutdown(2)
-    print("Success connecting to ")
-    print(ip," on port: ",str(port))
-except socket.error as e:
-    print("Cannot connect to ")
-    print(ip," on port: ",str(port))
-    print(e)
